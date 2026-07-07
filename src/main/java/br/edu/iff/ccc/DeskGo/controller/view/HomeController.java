16b33ea201bbff5ea04a44ec5d965d6279a5a0fb
Victória Zapata <victoriazapata.gz@gmail.com>
2026-07-07 01:06:23 +0000

    Adicionar controller e view Thymeleaf

--- src/main/java/br/edu/iff/ccc/DeskGo/controller/view/HomeController.java
+++ src/main/java/br/edu/iff/ccc/DeskGo/controller/view/HomeController.java
@@ -1,13 +1,28 @@
 package br.edu.iff.ccc.DeskGo.controller.view;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
-    @GetMapping({"/", "/home"})
-    public String home() {
+    @GetMapping({"/", "/home", "/inicial"})
+    public String exibirHome(Model model) {
+        model.addAttribute("mensagem", "Seja bem-vindo ao Web Project!");
+        return "home";
+    }
+
+    @GetMapping("/inicial/{id}")
+    public String exibirComParametro(
+            @PathVariable("id") String id,
+            @RequestParam(value = "param", required = false, defaultValue = "valor") String param,
+            Model model) {
+        model.addAttribute("id", id);
+        model.addAttribute("var1", param);
+        model.addAttribute("mensagem", "Página com parâmetros carregada com sucesso!");
         return "home";
     }
 }
---
+++ src/test/java/br/edu/iff/ccc/DeskGo/controller/view/HomeControllerTest.java
@@ -0,0 +1,11 @@
+package br.edu.iff.ccc.DeskGo.controller.view;
+
+import org.junit.jupiter.api.Test;
+
+class HomeControllerTest {
+
+    @Test
+    void exemploDeTesteSimples() {
+        assert true;
+    }
+}
